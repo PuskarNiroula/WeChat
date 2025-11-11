82d59e6e6d5b03a3d0af3c107f90e6f9de4c9d1b
PuskarDaju <puskarniroula8@gmail.com>
2025-11-11 19:09:44 +0545

    hello world

--- routes/channels.php
+++ routes/channels.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Log;
 Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
 Broadcast::channel('Message-Channel.{id}', function ($user, $id) {
-    Log::info('Authenticated user: ' . json_encode($user));
+    Log::info('Channel auth attempt', ['user_id' => $user?->id, 'channel_id' => $id]);
     return $user && (int) $user->id === (int) $id;
 });
 
