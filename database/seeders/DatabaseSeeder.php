acc4b4c88b83b1b9cf033aa85fd5134428ce6a78
Puskar Niroula <puskarniroula8@gmail.com>
2025-11-11 21:28:13 +0545

    seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,11 +19,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-//            UserSeeder::class,
-//            CoversationSeeder::class,
-//            CoversationUserSeeder::class,
-//            MessageSeeder::class,
-        LastSeeder::class,
+            UserSeeder::class,
+            CoversationSeeder::class,
+            CoversationUserSeeder::class,
+            MessageSeeder::class,
+            LastSeeder::class,
         ]);
     }
 }
